6b77915761f414d1c69f795655de5d727d4450b4
Joe McLean <mclean.joe@gmail.com>
2013-11-11 00:38:31 -0500

    Added remove button and alphabetic resorting.

--- app/assets/javascripts/setlists.js
+++ app/assets/javascripts/setlists.js
@@ -6,10 +6,34 @@ $(function() {
 
 
 function SongCtrl($scope) {
-  $scope.artist_songs = gon.songs
+
+  $scope.sortByKey = function(array, key) {
+    return array.sort(function(a, b) {
+        var x = a[key]; var y = b[key];
+        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+    });
+  }
+
+  $scope.artist_songs = gon.songs;
+  $scope.sortByKey($scope.artist_songs, 'name');
   $scope.setlist_songs = [];
 
-  $scope.addSongFromList = function(song) {
+  $scope.addSongToSetlist = function(song) {
     $scope.setlist_songs.push(song);
+    $scope.removeSongFromArray($scope.artist_songs, song);
   };
+
+  $scope.removeSongFromSetlist = function(song) {
+    $scope.artist_songs.push(song);
+    $scope.removeSongFromArray($scope.setlist_songs, song);
+    $scope.sortByKey($scope.artist_songs, 'name');
+  };
+
+  $scope.removeSongFromArray = function(array, song){
+    for(var i = array.length; i--;) {
+      if(array[i] === song) {
+          array.splice(i, 1);
+      }
+    }
+  }
 }
