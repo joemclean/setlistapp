9f56fba8ef37afe181b8e7d5b8c94b417bc0cc55
Joe McLean <mclean.joe@gmail.com>
2013-11-11 00:10:03 -0500

    Wiring up add song button

--- app/assets/javascripts/setlists.js
+++ app/assets/javascripts/setlists.js
@@ -7,11 +7,7 @@ $(function() {
 
 function SongCtrl($scope) {
   $scope.songs = gon.songs
-  $scope.addSong = function() {
-    $scope.songs.push({text:$scope.name});
-    $scope.songText = '';
-  };
-  $scope.addSongFromList = function(songName) {
-    $scope.songs.push({text:name});
+  $scope.addSongFromList = function(name) {
+    $scope.songs.push({name:name});
   };
 }
